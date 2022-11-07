dd47ec5ebde7a3d144a1b6057afd58f44517a9dd
itduki <mistolteen0127@gmail.com>
2022-11-07 14:40:27 +0900

    各セクションの表示内容を整理

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,6 @@
+import dayjs from 'dayjs'
+import Link from 'next/link'
+
 export type CardProps = {
   blogTitle: string
   blogUrl?: string
@@ -6,12 +9,28 @@ export type CardProps = {
   pubDate: string
 }
 
-export const Card: React.FC<CardProps> = (props) => {
+export const Card: React.FC<CardProps> = ({ blogTitle, blogUrl, articleTitle, articleUrl, pubDate }) => {
   return (
-    <ul>
-      {Object.entries(props).map(([key, value], index) => {
-        return <li key={index}>{`${key}-${value}`}</li>
-      })}
-    </ul>
+    <div>
+      <h1>
+        {articleUrl ? (
+          <Link href={articleUrl} prefetch={false} target="_blank">
+            <a>{articleTitle}</a>
+          </Link>
+        ) : (
+          articleTitle
+        )}
+      </h1>
+      <p>
+        {blogUrl ? (
+          <Link href={blogUrl} prefetch={false} target="_blank">
+            <a>{blogTitle}</a>
+          </Link>
+        ) : (
+          blogTitle
+        )}
+      </p>
+      <p>{dayjs(pubDate).format('YYYY-MM-DD')}</p>
+    </div>
   )
 }
